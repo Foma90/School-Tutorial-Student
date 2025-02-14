21e3828bdb39c24ef788e2bffcecb4779ebf5a64
yodatime <electriclikethor@gmail.com>
2025-02-14 11:37:19 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Proekt_Rep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
