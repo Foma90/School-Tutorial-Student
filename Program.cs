dfebf1a7c2181b769419c219a1cdcefbea2d246e
MEGA <MEGA@gmail.com>
2025-02-17 12:19:07 +0200

    add n

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
-﻿namespace Proekt_Rep
+﻿using System.Runtime.CompilerServices;
+
+namespace Proekt_Rep
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int n = 1;
             Print();
         }
 
         static void Print()
         {
-            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine($"{n++} {Console.ReadLine()}");
         }
+
+        
     }
 }
